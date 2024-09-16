185acd89c8e2a4b51b92b30b7adde8bb9c89d383
Crt Vavros <4377556+smlu@users.noreply.github.com>
2024-09-16 21:39:58 +0200

    Add missing  declaration for `stdPlatform_GetTimeMsec` & `stdPlatform_UnlockHandle`

--- Libs/std/General/stdPlatform.h
+++ Libs/std/General/stdPlatform.h
@@ -8,12 +8,14 @@ J3D_EXTERN_C_START
 
 int J3DAPI stdPlatform_InitServices(tHostServices* pHS);
 void J3DAPI stdPlatform_ClearServices(tHostServices* pHS);
+unsigned int stdPlatform_GetTimeMsec(void);
 J3DNORETURN void J3DAPI stdPlatform_Assert(const char* pErrorStr, const char* pFilename, int linenum);
 int stdPlatform_Printf(const char* format, ...);
-void* J3DAPI stdPlatform_AllocHandle(unsigned int size);
+void* J3DAPI stdPlatform_AllocHandle(size_t size);
 void J3DAPI stdPlatform_FreeHandle(void* pData);
-void* J3DAPI stdPlatform_ReallocHandle(void* pMemory, unsigned int newSize);
+void* J3DAPI stdPlatform_ReallocHandle(void* pMemory, size_t newSize);
 int J3DAPI stdPlatform_LockHandle(int a1);
+void J3DAPI stdPlatform_UnlockHandle();
 
 // Helper hooking functions
 void stdPlatform_InstallHooks(void);
