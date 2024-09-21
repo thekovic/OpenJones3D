20900a612ba0dd78bb64130eb65b17c32d61ac8e
Crt Vavros <4377556+smlu@users.noreply.github.com>
2024-09-21 22:42:00 +0200

    Implement `jonesString` & `JonesFile`

--- Jones3D/Main/JonesFile.c
+++ Jones3D/Main/JonesFile.c
@@ -2,163 +2,491 @@
 #include <j3dcore/j3dhook.h>
 #include <Jones3D/RTI/symbols.h>
 
-#define JonesFile_aResources J3D_DECL_FAR_ARRAYVAR(JonesFile_aResources, JonesResource(*)[3])
-#define JonesFile_sysHS J3D_DECL_FAR_VAR(JonesFile_sysHS, tHostServices)
-#define JonesFile_aPathBuf J3D_DECL_FAR_ARRAYVAR(JonesFile_aPathBuf, char(*)[48])
-#define JonesFile_aFileHandles J3D_DECL_FAR_ARRAYVAR(JonesFile_aFileHandles, JonesFileHandle(*)[33])
-#define JonesFile_bStartup J3D_DECL_FAR_VAR(JonesFile_bStartup, int)
-#define JonesFile_bOpened J3D_DECL_FAR_VAR(JonesFile_bOpened, int)
-#define JonesFile_pSysEnv J3D_DECL_FAR_VAR(JonesFile_pSysEnv, tHostServices*)
-#define JonesFile_pHS J3D_DECL_FAR_VAR(JonesFile_pHS, tHostServices*)
-#define JonesFile_hsInitCount J3D_DECL_FAR_VAR(JonesFile_hsInitCount, int)
+#include <std/General/std.h>
+#include <std/General/stdFnames.h>
+#include <std/General/stdFileUtil.h>
+#include <std/General/stdUtil.h>
+#include <std/Win95/stdGob.h>
+
+#define JF_MAX_HANDLES 32
+
+#define JF_RES_INSTALL 0
+#define JF_RES_CWD     1
+#define JF_RES_CD      2
+
+#define JF_ISVALID_FH(fh) (((fh) >= (1)) ? (((fh) <= (32 - 1)) ? 1 : 0) : 0)
+
+bool JonesFile_bStartup = false; // Added: init to false
+bool JonesFile_bOpened  = false; // Added: init to false
+
+int JonesFile_hsInitCount;
+tHostServices* JonesFile_pHS;
+tHostServices JonesFile_sysHS;
+tHostServices* JonesFile_pSysEnv;
+
+JonesResource JonesFile_aResources[3];
+
+char JonesFile_aPathBuf[128]; // Fixed: increased buffer size to 128, was 48
+JonesFileHandle JonesFile_aFileHandles[JF_MAX_HANDLES];
+
 
 void JonesFile_InstallHooks(void)
 {
-    // Uncomment only lines for functions that have full definition and doesn't call original function (non-thunk functions)
-
-    // J3D_HOOKFUNC(JonesFile_Startup);
-    // J3D_HOOKFUNC(JonesFile_Shutdown);
-    // J3D_HOOKFUNC(JonesFile_Open);
-    // J3D_HOOKFUNC(JonesFile_Close);
-    // J3D_HOOKFUNC(JonesFile_GetCurrentCDNum);
-    // J3D_HOOKFUNC(JonesFile_FileExists);
-    // J3D_HOOKFUNC(JonesFile_AddInstallPath);
-    // J3D_HOOKFUNC(JonesFile_AddCDPath);
-    // J3D_HOOKFUNC(JonesFile_InitServices);
-    // J3D_HOOKFUNC(JonesFile_ResetServices);
-    // J3D_HOOKFUNC(JonesFile_AddResourcePath);
-    // J3D_HOOKFUNC(JonesFile_CloseResource);
-    // J3D_HOOKFUNC(JonesFile_OpenFile);
-    // J3D_HOOKFUNC(JonesFile_CloseFile);
-    // J3D_HOOKFUNC(JonesFile_FileRead);
-    // J3D_HOOKFUNC(JonesFile_FileWrite);
-    // J3D_HOOKFUNC(JonesFile_FileGets);
-    // J3D_HOOKFUNC(JonesFile_FileEOF);
-    // J3D_HOOKFUNC(JonesFile_FileTell);
-    // J3D_HOOKFUNC(JonesFile_FileSeek);
-    // J3D_HOOKFUNC(JonesFile_FileSize);
-    // J3D_HOOKFUNC(JonesFile_FilePrintf);
+    J3D_HOOKFUNC(JonesFile_Startup);
+    J3D_HOOKFUNC(JonesFile_Shutdown);
+    J3D_HOOKFUNC(JonesFile_Open);
+    J3D_HOOKFUNC(JonesFile_Close);
+    J3D_HOOKFUNC(JonesFile_GetCurrentCDNum);
+    J3D_HOOKFUNC(JonesFile_FileExists);
+    J3D_HOOKFUNC(JonesFile_AddInstallPath);
+    J3D_HOOKFUNC(JonesFile_AddCDPath);
+    J3D_HOOKFUNC(JonesFile_InitServices);
+    J3D_HOOKFUNC(JonesFile_ResetServices);
+    J3D_HOOKFUNC(JonesFile_AddResourcePath);
+    J3D_HOOKFUNC(JonesFile_CloseResource);
+    J3D_HOOKFUNC(JonesFile_OpenFile);
+    J3D_HOOKFUNC(JonesFile_CloseFile);
+    J3D_HOOKFUNC(JonesFile_FileRead);
+    J3D_HOOKFUNC(JonesFile_FileWrite);
+    J3D_HOOKFUNC(JonesFile_FileGets);
+    J3D_HOOKFUNC(JonesFile_FileEOF);
+    J3D_HOOKFUNC(JonesFile_FileTell);
+    J3D_HOOKFUNC(JonesFile_FileSeek);
+    J3D_HOOKFUNC(JonesFile_FileSize);
+    J3D_HOOKFUNC(JonesFile_FilePrintf);
 }
 
 void JonesFile_ResetGlobals(void)
 {
-    memset(&JonesFile_aResources, 0, sizeof(JonesFile_aResources));
-    memset(&JonesFile_sysHS, 0, sizeof(JonesFile_sysHS));
-    memset(&JonesFile_aPathBuf, 0, sizeof(JonesFile_aPathBuf));
-    memset(&JonesFile_aFileHandles, 0, sizeof(JonesFile_aFileHandles));
-    memset(&JonesFile_bStartup, 0, sizeof(JonesFile_bStartup));
-    memset(&JonesFile_bOpened, 0, sizeof(JonesFile_bOpened));
-    memset(&JonesFile_pSysEnv, 0, sizeof(JonesFile_pSysEnv));
-    memset(&JonesFile_pHS, 0, sizeof(JonesFile_pHS));
-    memset(&JonesFile_hsInitCount, 0, sizeof(JonesFile_hsInitCount));
+
 }
 
 void J3DAPI JonesFile_Startup(tHostServices* pHS)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_Startup, pHS);
+    if ( !JonesFile_bStartup )
+    {
+        stdGob_Startup(pHS);
+        JonesFile_hsInitCount = 0;
+        JonesFile_bStartup    = true;
+    }
 }
 
 void JonesFile_Shutdown(void)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_Shutdown);
+    if ( JonesFile_bStartup )
+    {
+        stdGob_Shutdown();
+        JonesFile_hsInitCount = 0;
+        JonesFile_bStartup    = false;
+    }
 }
 
 int J3DAPI JonesFile_Open(tHostServices* pEnv, const char* pInstallPath, const char* pPathCD)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_Open, pEnv, pInstallPath, pPathCD);
+    if ( !JonesFile_bStartup )
+    {
+        return 1;
+    }
+
+    if ( JonesFile_bOpened )
+    {
+        STDLOG_ERROR("Warning: System already open!\n");
+        return 1;
+    }
+    else
+    {
+        memcpy(&JonesFile_sysHS, pEnv, sizeof(JonesFile_sysHS));
+        JonesFile_pHS     = &JonesFile_sysHS;
+        JonesFile_pSysEnv = pEnv;
+
+        JonesFile_InitServices(); // This will override current pEnv
+
+        // Add resource paths:
+        // 0 - install path
+        // 1 - current work dir
+        // 2 - CD path
+        memset(JonesFile_aResources, 0, sizeof(JonesFile_aResources));
+
+        if ( pInstallPath ) {
+            JonesFile_AddInstallPath(pInstallPath);
+        }
+
+        if ( pPathCD ) {
+            JonesFile_AddCDPath(pPathCD);
+        }
+
+        // Add path to current working dir
+        JonesFile_AddResourcePath(&JonesFile_aResources[JF_RES_CWD], ".");
+
+        memset(JonesFile_aFileHandles, 0, sizeof(JonesFile_aFileHandles));
+
+        JonesFile_bOpened = true;
+        return 0;
+    }
 }
 
 void J3DAPI JonesFile_Close()
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_Close);
+    if ( !JonesFile_bOpened )
+
+    {
+        STDLOG_ERROR("Warning: System already closed!\n");
+        return;
+    }
+
+    for ( size_t i = 0; i < JF_MAX_HANDLES; ++i )
+    {
+        if ( JonesFile_aFileHandles[i].bUsed )
+        {
+            JonesFile_CloseFile(i + 1);
+            STDLOG_WARNING("File left open %s. Closed now!\n", JonesFile_aFileHandles[i].aName); // Changed: Changed to warn log from error log. Added 'Closed now!'
+        }
+    }
+
+    JonesFile_ResetServices();
+    JonesFile_CloseResource(&JonesFile_aResources[JF_RES_INSTALL]);
+    JonesFile_CloseResource(&JonesFile_aResources[JF_RES_CD]);
+    JonesFile_CloseResource(&JonesFile_aResources[JF_RES_CWD]);
+    JonesFile_bOpened = false;
 }
 
 int J3DAPI JonesFile_GetCurrentCDNum()
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_GetCurrentCDNum);
+    char aPath[128];
+    stdFnames_MakePath(aPath, sizeof(aPath), JonesFile_aResources[JF_RES_CD].dirPath, "CD1.GOB");
+    if ( std_g_pHS->pFileSize(aPath) )
+    {
+        return 1;
+    }
+
+    stdFnames_MakePath(aPath, sizeof(aPath), JonesFile_aResources[JF_RES_CD].dirPath, "CD2.GOB");
+    if ( std_g_pHS->pFileSize(aPath) )
+    {
+        return 2;
+    }
+
+    return 0;
 }
 
 int J3DAPI JonesFile_FileExists(const char* pFilename)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileExists, pFilename);
+    tFileHandle fh = std_g_pHS->pFileOpen(pFilename, "rb");
+    if ( !fh ) {
+        return 0;
+    }
+
+    std_g_pHS->pFileClose(fh);
+    return 1;
 }
 
 void J3DAPI JonesFile_AddInstallPath(const char* pPath)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_AddInstallPath, pPath);
+    JonesFile_CloseResource(&JonesFile_aResources[JF_RES_INSTALL]);
+    if ( *pPath ) {
+        JonesFile_AddResourcePath(&JonesFile_aResources[JF_RES_INSTALL], pPath);
+    }
 }
 
 void J3DAPI JonesFile_AddCDPath(const char* pPath)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_AddCDPath, pPath);
+
+    JonesFile_CloseResource(&JonesFile_aResources[JF_RES_CD]);
+    if ( *pPath )
+    {
+        char aPath[128];
+        STD_FORMAT(aPath, "%sresource", pPath);
+        JonesFile_AddResourcePath(&JonesFile_aResources[JF_RES_CD], aPath);
+    }
+    else
+    {
+        STDLOG_WARNING("Empty CD path specified.\n");
+    }
 }
 
 void JonesFile_InitServices(void)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_InitServices);
+    if ( !JonesFile_hsInitCount )
+    {
+        JonesFile_pSysEnv->pFileOpen   = JonesFile_OpenFile;
+        JonesFile_pSysEnv->pFileClose  = JonesFile_CloseFile;
+        JonesFile_pSysEnv->pFileRead   = JonesFile_FileRead;
+        JonesFile_pSysEnv->pFileGets   = JonesFile_FileGets;
+        JonesFile_pSysEnv->pFileWrite  = JonesFile_FileWrite;
+        JonesFile_pSysEnv->pFileEOF    = JonesFile_FileEOF;
+        JonesFile_pSysEnv->pFileTell   = JonesFile_FileTell;
+        JonesFile_pSysEnv->pFileSeek   = JonesFile_FileSeek;
+        JonesFile_pSysEnv->pFileSize   = JonesFile_FileSize;
+        JonesFile_pSysEnv->pFilePrintf = JonesFile_FilePrintf;
+    }
+
+    ++JonesFile_hsInitCount;
 }
 
 void JonesFile_ResetServices(void)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_ResetServices);
+    if ( JonesFile_hsInitCount )
+    {
+        JonesFile_pSysEnv->pFileOpen   = JonesFile_sysHS.pFileOpen;
+        JonesFile_pSysEnv->pFileClose  = JonesFile_sysHS.pFileClose;
+        JonesFile_pSysEnv->pFileRead   = JonesFile_sysHS.pFileRead;
+        JonesFile_pSysEnv->pFileGets   = JonesFile_sysHS.pFileGets;
+        JonesFile_pSysEnv->pFileWrite  = JonesFile_sysHS.pFileWrite;
+        JonesFile_pSysEnv->pFileEOF    = JonesFile_sysHS.pFileEOF;
+        JonesFile_pSysEnv->pFileTell   = JonesFile_sysHS.pFileTell;
+        JonesFile_pSysEnv->pFileSeek   = JonesFile_sysHS.pFileSeek;
+        JonesFile_pSysEnv->pFileSize   = JonesFile_sysHS.pFileSize;
+        JonesFile_pSysEnv->pFilePrintf = JonesFile_sysHS.pFilePrintf;
+        --JonesFile_hsInitCount;
+    }
 }
 
 void J3DAPI JonesFile_AddResourcePath(JonesResource* pRsource, const char* pPath)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_AddResourcePath, pRsource, pPath);
+    STD_STRCPY(pRsource->dirPath, pPath);
+    JonesFile_ResetServices();
+
+    // Now find all *.gob files in dir
+    pRsource->numGobFiles = 0;
+    FindFileData* pFileData = stdFileUtil_NewFind(pPath, /*mode=*/3, "gob");
+    tFoundFileInfo pFile;
+    while ( stdFileUtil_FindNext(pFileData, &pFile) )
+    {
+        if ( pFile.aName[0] != '.' )
+        {
+            stdFnames_MakePath(JonesFile_aPathBuf, sizeof(JonesFile_aPathBuf), pPath, pFile.aName);
+            pRsource->aGobFiles[pRsource->numGobFiles] = stdGob_Load(JonesFile_aPathBuf, /*numFileHandles*/16, /*bMapFile*/0); // TODO: After file mapping is fixed in stdGob enable file mapping here
+            if ( !pRsource->aGobFiles[pRsource->numGobFiles] )
+            {
+                STDLOG_ERROR("Error opening gob '%s'.\n", JonesFile_aPathBuf);
+                break;
+            }
+
+            ++pRsource->numGobFiles;
+        }
+    }
+
+    stdFileUtil_DisposeFind(pFileData);
+    JonesFile_InitServices();
 }
 
 void J3DAPI JonesFile_CloseResource(JonesResource* pResource)
 {
-    J3D_TRAMPOLINE_CALL(JonesFile_CloseResource, pResource);
+    for ( size_t i = 0; i < pResource->numGobFiles; ++i )
+    {
+        stdGob_Free(pResource->aGobFiles[i]);
+    }
+
+    pResource->numGobFiles = 0;
 }
 
 tFileHandle J3DAPI JonesFile_OpenFile(const char* pFilename, const char* mode)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_OpenFile, pFilename, mode);
+    size_t handle;
+    for ( handle = 0; handle < JF_MAX_HANDLES && JonesFile_aFileHandles[handle].bUsed; ++handle )
+    {
+        ;
+    }
+
+    if ( handle >= JF_MAX_HANDLES )
+    {
+        STDLOG_ERROR("Out of file handles!\n");
+        return 0;
+    }
+
+    // Try to open file by provided path
+    bool bFileFound = false;
+    tFileHandle fh = JonesFile_pHS->pFileOpen(pFilename, mode);
+    if ( fh )
+    {
+        bFileFound = true;
+        JonesFile_aFileHandles[handle].bFileHandle = 1;
+        JonesFile_aFileHandles[handle].hFile       = fh;
+        JonesFile_aFileHandles[handle].bUsed       = 1;
+        STD_STRCPY(JonesFile_aFileHandles[handle].aName, pFilename);
+    }
+    else // No file was found, try to find it in stored resources
+    {
+        // Search the resources:
+        // 0 - install path
+        // 1 - current work dir path
+        // 2 - CD path
+        JonesResource* pCurRes = JonesFile_aResources;
+        for ( size_t resNum = 0; resNum < STD_ARRAYLEN(JonesFile_aResources) && !bFileFound; ++resNum )
+        {
+            if ( pCurRes->dirPath[0] )
+            {
+                if ( pCurRes != &JonesFile_aResources[JF_RES_CD] )// If not CD resource
+                {
+                    stdFnames_MakePath(JonesFile_aPathBuf, sizeof(JonesFile_aPathBuf), pCurRes->dirPath, pFilename);
+                    fh = JonesFile_pHS->pFileOpen(JonesFile_aPathBuf, mode);
+                    if ( fh )
+                    {
+                        bFileFound = true;
+                        JonesFile_aFileHandles[handle].bFileHandle = 1;
+                        JonesFile_aFileHandles[handle].hFile       = fh;
+                        JonesFile_aFileHandles[handle].bUsed       = 1;
+                        STD_STRCPY(JonesFile_aFileHandles[handle].aName, JonesFile_aPathBuf);
+                    }
+                }
+
+                if ( !bFileFound )
+                {
+                    // File was not found on disk try to search for file in GOB resources
+                    for ( size_t gobNum = 0; gobNum < pCurRes->numGobFiles && !bFileFound; ++gobNum )
+                    {
+                        GobFileHandle* pGFH = stdGob_FileOpen(pCurRes->aGobFiles[gobNum], pFilename);
+                        if ( pGFH )
+                        {
+                            bFileFound = true;
+                            JonesFile_aFileHandles[handle].bFileHandle    = 0;
+                            JonesFile_aFileHandles[handle].pGobFileHandle = pGFH;
+                            JonesFile_aFileHandles[handle].bUsed          = 1;
+                            STD_STRCPY(JonesFile_aFileHandles[handle].aName, pFilename);
+                        }
+                    }
+                }
+            }
+
+            ++pCurRes;
+        }
+    }
+
+    if ( bFileFound )
+    {
+        return handle + 1; // Note, file handle gets incremented here by one... I guess so the valid handle is always greater than 0
+    }
+
+    return 0;
 }
 
 int J3DAPI JonesFile_CloseFile(tFileHandle fh)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_CloseFile, fh);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];  // Note, fh has to be decremented here because OpenFile incremented it by 1 so the valid handle is always greater than 0
+    if ( pHandle->bUsed )
+    {
+        if ( pHandle->bFileHandle )
+        {
+            JonesFile_pHS->pFileClose(pHandle->hFile);
+            pHandle->hFile = 0;
+        }
+        else
+        {
+            stdGob_FileClose(pHandle->pGobFileHandle);
+            pHandle->pGobFileHandle = NULL;
+        }
+    }
+
+    memset(pHandle, 0, sizeof(JonesFileHandle));
+    return 0;
 }
 
 size_t J3DAPI JonesFile_FileRead(tFileHandle fh, void* pDst, size_t size)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileRead, fh, pDst, size);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];
+    if ( pHandle->bFileHandle )
+    {
+        return JonesFile_pHS->pFileRead(pHandle->hFile, pDst, size);
+    }
+    else
+    {
+        return stdGob_FileRead(pHandle->pGobFileHandle, pDst, size);
+    }
 }
 
 size_t J3DAPI JonesFile_FileWrite(tFileHandle fh, const void* pData, size_t size)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileWrite, fh, pData, size);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];
+    if ( pHandle->bFileHandle )
+    {
+        return JonesFile_pHS->pFileWrite(pHandle->hFile, pData, size);
+    }
+
+    STDLOG_ERROR("Error tried to write to bundled file '%s'\n", pHandle->aName);
+    return 0;
 }
 
 char* J3DAPI JonesFile_FileGets(tFileHandle fh, char* pStr, size_t size)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileGets, fh, pStr, size);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];
+    if ( pHandle->bFileHandle )
+    {
+        return JonesFile_pHS->pFileGets(pHandle->hFile, pStr, size);
+    }
+    else
+    {
+        return (char*)stdGob_FileGets(pHandle->pGobFileHandle, pStr, size);
+    }
 }
 
 int J3DAPI JonesFile_FileEOF(tFileHandle fh)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileEOF, fh);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];
+    if ( pHandle->bFileHandle )
+    {
+        return JonesFile_pHS->pFileEOF(pHandle->hFile);
+    }
+    else
+    {
+        return stdGob_FileEOF(pHandle->pGobFileHandle);
+    }
 }
 
 int J3DAPI JonesFile_FileTell(tFileHandle fh)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileTell, fh);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];
+    if ( pHandle->bFileHandle )
+    {
+        return JonesFile_pHS->pFileTell(pHandle->hFile);
+    }
+    else
+    {
+        return stdGob_FileTell(pHandle->pGobFileHandle);
+    }
 }
 
 int J3DAPI JonesFile_FileSeek(tFileHandle fh, int offset, int origin)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileSeek, fh, offset, origin);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];
+    if ( pHandle->bFileHandle )
+    {
+        return JonesFile_pHS->pFileSeek(pHandle->hFile, offset, origin);
+    }
+    else
+    {
+        return stdGob_FileSeek(pHandle->pGobFileHandle, offset, origin);
+    }
 }
 
 size_t J3DAPI JonesFile_FileSize(const char* pFilename)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FileSize, pFilename);
+    return JonesFile_pHS->pFileSize(pFilename);
 }
 
 int JonesFile_FilePrintf(tFileHandle fh, const char* aFormat, ...)
 {
-    return J3D_TRAMPOLINE_CALL(JonesFile_FilePrintf, fh, aFormat);
+    STD_ASSERTREL(JF_ISVALID_FH(fh));
+
+    va_list args;
+    va_start(args, aFormat);
+    size_t size = vsnprintf(std_g_genBuffer, sizeof(std_g_genBuffer), aFormat, args);
+    va_end(args);
+
+    JonesFileHandle* pHandle = &JonesFile_aFileHandles[fh - 1];
+    if ( pHandle->bFileHandle )
+    {
+        JonesFile_pHS->pFileWrite(pHandle->hFile, std_g_genBuffer, size); // Fixed: Fixed writing to file by using pFileWrite function instead of pFilePrintf
+    }
+    else
+    {
+        STDLOG_ERROR("Error tried to printf to bundled file '%s'\n", pHandle->aName);
+    }
+
+    return 0;
 }
--- Jones3D/Main/jonesString.c
+++ Jones3D/Main/jonesString.c
@@ -2,37 +2,73 @@
 #include <j3dcore/j3dhook.h>
 #include <Jones3D/RTI/symbols.h>
 
-#define jonesString_jonesStringsTbl J3D_DECL_FAR_VAR(jonesString_jonesStringsTbl, tStringTable)
-#define jonesString_aBuffer J3D_DECL_FAR_ARRAYVAR(jonesString_aBuffer, char(*)[512])
-#define jonesString_bStarted J3D_DECL_FAR_VAR(jonesString_bStarted, int)
+#include <std/General/std.h>
+#include <std/General/stdMemory.h>
+#include <std/General/stdStrTable.h>
+#include <std/General/stdUtil.h>
+
+static tStringTable jonesString_jonesStringsTbl;
+static char jonesString_aBuffer[512] = { 0 };
+static bool jonesString_bStarted = false;
 
 void jonesString_InstallHooks(void)
 {
-    // Uncomment only lines for functions that have full definition and doesn't call original function (non-thunk functions)
-
-    // J3D_HOOKFUNC(jonesString_GetString);
-    // J3D_HOOKFUNC(jonesString_Startup);
-    // J3D_HOOKFUNC(jonesString_Shutdown);
+    J3D_HOOKFUNC(jonesString_GetString);
+    J3D_HOOKFUNC(jonesString_Startup);
+    J3D_HOOKFUNC(jonesString_Shutdown);
 }
 
 void jonesString_ResetGlobals(void)
 {
-    memset(&jonesString_jonesStringsTbl, 0, sizeof(jonesString_jonesStringsTbl));
-    memset(&jonesString_aBuffer, 0, sizeof(jonesString_aBuffer));
-    memset(&jonesString_bStarted, 0, sizeof(jonesString_bStarted));
+    //memset(&jonesString_jonesStringsTbl, 0, sizeof(jonesString_jonesStringsTbl));
+    //memset(&jonesString_aBuffer, 0, sizeof(jonesString_aBuffer));
+    //memset(&jonesString_bStarted, 0, sizeof(jonesString_bStarted));
 }
 
 const char* J3DAPI jonesString_GetString(const char* pKey)
 {
-    return J3D_TRAMPOLINE_CALL(jonesString_GetString, pKey);
+    if ( !jonesString_bStarted ) {
+        return NULL;
+    }
+
+    if ( !pKey ) {
+        return NULL;
+    }
+
+    wchar_t* pWString = stdStrTable_GetValueOrKey(&jonesString_jonesStringsTbl, pKey);
+    if ( !pWString )
+    {
+        STD_STRCPY(jonesString_aBuffer, pKey);
+        return jonesString_aBuffer;
+    }
+
+    char* pAString = stdUtil_ToAString(pWString);
+    STD_STRCPY(jonesString_aBuffer, pAString);
+    stdMemory_Free(pAString);
+    return jonesString_aBuffer;
 }
 
 int J3DAPI jonesString_Startup()
 {
-    return J3D_TRAMPOLINE_CALL(jonesString_Startup);
+    if ( jonesString_bStarted ) {
+        return 0;
+    }
+
+    if ( !stdStrTable_Load(&jonesString_jonesStringsTbl, "misc\\jonesStrings.uni") )
+    {
+        STDLOG_STATUS("couldn't find file misc\\jonesStrings.uni\n");
+        return 1;
+    }
+
+    jonesString_bStarted = true;
+    return 0;
 }
 
 void jonesString_Shutdown(void)
 {
-    J3D_TRAMPOLINE_CALL(jonesString_Shutdown);
+    if ( jonesString_bStarted )
+    {
+        stdStrTable_Free(&jonesString_jonesStringsTbl);
+        jonesString_bStarted = false;
+    }
 }
