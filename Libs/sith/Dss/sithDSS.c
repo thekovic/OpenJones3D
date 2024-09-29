b5ed2e8f9daa2c4347f7dbec679193a168950f43
Crt Vavros <4377556+smlu@users.noreply.github.com>
2024-09-29 15:14:37 +0200

    [sith] Implement sithDSS_PuppetStatus & sithDSS_ProcessPuppetStatus
    Implementation fixes issues with serializing and de-serializing puppet callback info

--- Libs/sith/Dss/sithDSS.c
+++ Libs/sith/Dss/sithDSS.c
@@ -1,13 +1,45 @@
 #include "sithDSS.h"
 #include <j3dcore/j3dhook.h>
-#include <sith/RTI/symbols.h>
+
+#include <rdroid/Engine/rdPuppet.h>
+
+#include <sith/AI/sithAIMove.h>
+#include <sith/Devices/sithComm.h>
+#include <sith/Dss/sithMulti.h>
 #include <sith/Engine/sithPuppet.h>
 #include <sith/Gameplay/sithPlayerControls.h>
 #include <sith/Gameplay/sithVehicleControls.h>
 #include <sith/Gameplay/sithWhip.h>
-#include <sith/AI/sithAIMove.h>
+#include <sith/RTI/symbols.h>
+#include <sith/World/sithThing.h>
+#include <sith/World/sithWorld.h>
 
-#define sithDSS_aPuppetCallbacks J3D_DECL_FAR_ARRAYVAR(sithDSS_aPuppetCallbacks, rdPuppetTrackCallback(*)[7])
+#include <std/General/stdUtil.h>
+
+
+#define VANILLACB(callback) ((rdPuppetTrackCallback)(callback##_ADDR))
+
+static const rdPuppetTrackCallback sithDSS_aPuppetCallbacks[7][2] = {
+    { &sithPuppet_DefaultCallback,           VANILLACB(sithPuppet_DefaultCallback)           },
+    { &sithPlayerControls_PuppetCallback,    VANILLACB(sithPlayerControls_PuppetCallback)    },
+    { &sithVehicleControls_PuppetCallback,   VANILLACB(sithVehicleControls_PuppetCallback)   },
+    { &sithAIMove_PuppetCallback,            VANILLACB(sithAIMove_PuppetCallback)            },
+    { &sithWhip_WhipClimbPuppetCallback,     VANILLACB(sithWhip_WhipClimbPuppetCallback)     },
+    { &sithWhip_ClimbDismountPuppetCallback, VANILLACB(sithWhip_ClimbDismountPuppetCallback) },
+    { &sithWhip_WhipFirePuppetCallback,      VANILLACB(sithWhip_WhipFirePuppetCallback)      }
+};
+
+// TODO: When all functions that are using following callbacks are defined, uncomment below const and remove previous definition above 
+//static const rdPuppetTrackCallback sithDSS_aPuppetCallbacks[7] = {
+//    &sithPuppet_DefaultCallback,
+//    &sithPlayerControls_PuppetCallback,
+//    &sithVehicleControls_PuppetCallback,
+//    &sithAIMove_PuppetCallback,
+//    &sithWhip_WhipClimbPuppetCallback,
+//    &sithWhip_ClimbDismountPuppetCallback,
+//    &sithWhip_WhipFirePuppetCallback
+//};
+
 
 void sithDSS_InstallHooks(void)
 {
@@ -27,8 +59,8 @@ void sithDSS_InstallHooks(void)
     // J3D_HOOKFUNC(sithDSS_ProcessInventory);
     // J3D_HOOKFUNC(sithDSS_AnimStatus);
     // J3D_HOOKFUNC(sithDSS_ProcessAnimStatus);
-    // J3D_HOOKFUNC(sithDSS_PuppetStatus);
-    // J3D_HOOKFUNC(sithDSS_ProcessPuppetStatus);
+    J3D_HOOKFUNC(sithDSS_PuppetStatus);
+    J3D_HOOKFUNC(sithDSS_ProcessPuppetStatus);
     // J3D_HOOKFUNC(sithDSS_SyncTaskEvents);
     // J3D_HOOKFUNC(sithDSS_ProcessSyncTaskEvents);
     // J3D_HOOKFUNC(sithDSS_SyncCameras);
@@ -43,17 +75,6 @@ void sithDSS_InstallHooks(void)
 
 void sithDSS_ResetGlobals(void)
 {
-    rdPuppetTrackCallback sithDSS_aPuppetCallbacks_tmp[7] = {
-      &sithPuppet_DefaultCallback,
-      &sithPlayerControls_PuppetCallback,
-      &sithVehicleControls_PuppetCallback,
-      &sithAIMove_PuppetCallback,
-      &sithWhip_WhipClimbPuppetCallback,
-      &sithWhip_ClimbDismountPuppetCallback,
-      &sithWhip_WhipFirePuppetCallback
-    };
-    memcpy(&sithDSS_aPuppetCallbacks, &sithDSS_aPuppetCallbacks_tmp, sizeof(sithDSS_aPuppetCallbacks));
-    
 }
 
 int J3DAPI sithDSS_SurfaceStatus(const SithSurface* pSurf, DPID idTo, unsigned int outstream)
@@ -126,15 +147,15 @@ int J3DAPI sithDSS_ProcessAnimStatus(const SithMessage* pMsg)
     return J3D_TRAMPOLINE_CALL(sithDSS_ProcessAnimStatus, pMsg);
 }
 
-int J3DAPI sithDSS_PuppetStatus(const SithThing* pThing, DPID idTo, unsigned int outstream)
-{
-    return J3D_TRAMPOLINE_CALL(sithDSS_PuppetStatus, pThing, idTo, outstream);
-}
-
-int J3DAPI sithDSS_ProcessPuppetStatus(const SithMessage* pMsg)
-{
-    return J3D_TRAMPOLINE_CALL(sithDSS_ProcessPuppetStatus, pMsg);
-}
+//int J3DAPI sithDSS_PuppetStatus(const SithThing* pThing, DPID idTo, unsigned int outstream)
+//{
+//    return J3D_TRAMPOLINE_CALL(sithDSS_PuppetStatus, pThing, idTo, outstream);
+//}
+//
+//int J3DAPI sithDSS_ProcessPuppetStatus(const SithMessage* pMsg)
+//{
+//    return J3D_TRAMPOLINE_CALL(sithDSS_ProcessPuppetStatus, pMsg);
+//}
 
 int J3DAPI sithDSS_SyncTaskEvents(const SithEvent* pEvent, DPID idTo, unsigned int outstream)
 {
@@ -184,4 +205,273 @@ int J3DAPI sithDSS_sub_4B3760(DPID idTo, unsigned int outstream)
 int J3DAPI sithDSS_sub_4B3790(const SithMessage* pMsg)
 {
     return J3D_TRAMPOLINE_CALL(sithDSS_sub_4B3790, pMsg);
+}
+
+
+int J3DAPI sithDSS_PuppetStatus(const SithThing* pThing, DPID idTo, unsigned int outstream)
+{
+    SITH_ASSERTREL(pThing && pThing->renderData.pPuppet);
+    rdPuppet* pPuppet = pThing->renderData.pPuppet;
+
+    SITHDSS_STARTOUT(SITHDSS_PUPPETSTATUS);
+
+    SITHDSS_PUSHINT32(sithThing_GetThingIndex(pThing));
+    //*(int32_t*)sithMulti_g_message.data = sithThing_GetThingIndex(pThing);
+
+    //uint8_t* pCurOut = &sithMulti_g_message.data[4];
+
+    size_t trackCount = 0;
+    for ( size_t i = 0; i < STD_ARRAYLEN(pPuppet->aTracks); ++i )
+    {
+        SITHDSS_PUSHINT32(pPuppet->aTracks[i].status);
+        /**(int32_t*)pCurOut = pPuppet->aTracks[i].status;
+        pCurOut += 4;*/
+
+        if ( pPuppet->aTracks[i].status )
+        {
+            SITH_ASSERTREL(pPuppet->aTracks[i].pKFTrack);
+
+            SITHDSS_PUSHINT32(pPuppet->aTracks[i].pKFTrack->idx);
+            /**(int32_t*)pCurOut = pPuppet->aTracks[i].pKFTrack->idx;
+            pCurOut += 4;*/
+
+            SITHDSS_PUSHINT32(pPuppet->aTracks[i].unknown0);
+            /**(int32_t*)pCurOut = pPuppet->aTracks[i].unknown0;
+            pCurOut += 4;*/
+
+            SITHDSS_PUSHINT16(pPuppet->aTracks[i].lowPriority);
+            /**(int16_t*)pCurOut = (uint16_t)pPuppet->aTracks[i].lowPriority;
+            pCurOut += 2;*/
+
+            SITHDSS_PUSHINT16(pPuppet->aTracks[i].highPriority);
+
+            /**(int16_t*)pCurOut = (uint16_t)pPuppet->aTracks[i].highPriority;
+            pCurOut += 2;*/
+
+            SITHDSS_PUSHFLOAT(pPuppet->aTracks[i].fps);
+            /**(float*)pCurOut = pPuppet->aTracks[i].fps;
+            pCurOut += 4;*/
+
+            SITHDSS_PUSHFLOAT(pPuppet->aTracks[i].playSpeed);
+            /* *(float*)pCurOut = pPuppet->aTracks[i].playSpeed;
+             pCurOut += 4;*/
+
+            SITHDSS_PUSHFLOAT(pPuppet->aTracks[i].curFrame);
+            /**(float*)pCurOut = pPuppet->aTracks[i].curFrame;
+            pCurOut += 4;*/
+
+            SITHDSS_PUSHFLOAT(pPuppet->aTracks[i].prevFrame);
+            /**(float*)pCurOut = pPuppet->aTracks[i].prevFrame;
+            pCurOut += 4;*/
+
+            if ( pPuppet->aTracks[i].pfCallback )
+            {
+                size_t cbIdx = 0;
+                for ( ; cbIdx < STD_ARRAYLEN(sithDSS_aPuppetCallbacks); ++cbIdx )
+                {
+                    if ( pPuppet->aTracks[i].pfCallback == sithDSS_aPuppetCallbacks[cbIdx][0]
+                        || pPuppet->aTracks[i].pfCallback == sithDSS_aPuppetCallbacks[cbIdx][1] )
+                    {
+                        SITHDSS_PUSHUINT8(cbIdx);
+                        //*pCurOut++ = (uint8_t)cbIdx;
+                        break;
+                    }
+                }
+
+                if ( cbIdx == STD_ARRAYLEN(sithDSS_aPuppetCallbacks) )
+                {
+                    SITHLOG_ERROR("Saving Unknown callback for %s\n", pPuppet->aTracks[i].pKFTrack->aName);
+                    SITHDSS_PUSHUINT8(STD_ARRAYLEN(sithDSS_aPuppetCallbacks)); // mark there is no callback
+                    //*pCurOut++ = STD_ARRAYLEN(sithDSS_aPuppetCallbacks);
+                }
+            }
+            else // mark there is no callback
+            {
+                SITHDSS_PUSHUINT8(STD_ARRAYLEN(sithDSS_aPuppetCallbacks));
+                //*pCurOut++ = STD_ARRAYLEN(sithDSS_aPuppetCallbacks);
+            }
+
+            ++trackCount;
+        }
+    }
+
+    if ( pThing->pPuppetState )
+    {
+        SITHDSS_PUSHINT16(pThing->pPuppetState->armedMode);
+        /**(int16_t*)pCurOut = (int16_t)pThing->pPuppetState->armedMode;
+        pCurOut += 2;*/
+
+        SITHDSS_PUSHINT16(pThing->pPuppetState->moveMode);
+        /* *(int16_t*)pCurOut = (int16_t)pThing->pPuppetState->moveMode;
+         pCurOut += 2;*/
+
+        SITHDSS_PUSHINT16(trackCount);
+
+        /**(int16_t*)pCurOut = (int16_t)trackCount;
+        pCurOut += 2;*/
+
+        SithPuppetTrack* pCurTrack = pThing->pPuppetState->pFirstTrack;
+        while ( trackCount )
+        {
+            if ( pCurTrack )
+            {
+                SITHDSS_PUSHINT32(pCurTrack->trackNum);
+                /**(int32_t*)pCurOut = pCurTrack->trackNum;
+                pCurOut += 4;*/
+
+                SITHDSS_PUSHUINT32(pCurTrack->submode);
+                /**(uint32_t*)pCurOut = pCurTrack->submode;
+                pCurOut += 4;*/
+
+                pCurTrack = pCurTrack->pNextTrack;
+            }
+            else // If no track
+            {
+                SITHDSS_PUSHUINT32(STD_ARRAYLEN(pPuppet->aTracks)); // mark null track
+                /**(uint32_t*)pCurOut = STD_ARRAYLEN(pPuppet->aTracks);
+                pCurOut += 4;*/
+
+                SITHDSS_PUSHUINT32(SITH_PUPPET_NUMSUBMODES); // mark no mode
+                /**(uint32_t*)pCurOut = SITH_PUPPET_NUMSUBMODES;
+                pCurOut += 4;*/
+            }
+
+            --trackCount;
+        }
+    }
+
+    /*sithMulti_g_message.type = SITHDSS_PUPPETSTATUS;
+    sithMulti_g_message.length = pCurOut - sithMulti_g_message.data;*/
+    SITHDSS_ENDOUT;
+    return sithMessage_SendMessage(&sithMulti_g_message, idTo, outstream, 1u);
+}
+
+int J3DAPI sithDSS_ProcessPuppetStatus(const SithMessage* pMsg)
+{
+    SITH_ASSERTREL(pMsg);
+
+    SITHDSS_STARTIN(pMsg);
+
+    SithThing* pThing = sithThing_GetThingByIndex(SITHDSS_POPINT32());
+    //uint8_t* pCurIn   = (uint8_t*)&pMsg->data[4];
+    //SithThing* pThing = sithThing_GetThingByIndex(*(int32_t*)pMsg->data);
+
+    if ( !pThing || !pThing->pPuppetClass || !pThing->pPuppetState || !pThing->renderData.pPuppet )
+    {
+        SITHLOG_ERROR("Error in DSS::PuppetStatus message format.\n");
+        SITHDSS_ENDIN;
+        return 0;
+    }
+
+    rdPuppet* pPuppet = pThing->renderData.pPuppet;
+    rdPuppet_NewEntry(pPuppet, &pThing->renderData);
+
+    for ( size_t i = 0; i < STD_ARRAYLEN(pPuppet->aTracks); ++i )
+    {
+        pPuppet->aTracks[i].status = SITHDSS_POPINT32();
+        /*pPuppet->aTracks[i].status = *(int32_t*)pCurIn;
+        pCurIn += 4;*/
+
+        if ( pPuppet->aTracks[i].status )
+        {
+            int kfIdx = SITHDSS_POPINT32();
+            /*int kfIdx = *(int32_t*)pCurIn;
+            pCurIn += 4;*/
+
+            pPuppet->aTracks[i].pKFTrack = sithPuppet_GetKeyframeByIndex(kfIdx);
+
+            pPuppet->aTracks[i].unknown0 = SITHDSS_POPINT32();
+            /*pPuppet->aTracks[i].unknown0 = *(int32_t*)pCurIn;
+            pCurIn += 4;*/
+
+            pPuppet->aTracks[i].lowPriority = SITHDSS_POPINT16();
+            /*pPuppet->aTracks[i].lowPriority = *(int16_t*)pCurIn;
+            pCurIn += 2;*/
+
+            pPuppet->aTracks[i].highPriority = SITHDSS_POPINT16();
+            /* pPuppet->aTracks[i].highPriority = *(int16_t*)pCurIn;
+             pCurIn += 2;*/
+
+            pPuppet->aTracks[i].fps = SITHDSS_POPFLOAT();
+            /*pPuppet->aTracks[i].fps = *(float*)pCurIn;
+            pCurIn += 4;*/
+
+            pPuppet->aTracks[i].playSpeed = SITHDSS_POPFLOAT();
+            /*pPuppet->aTracks[i].playSpeed = *(float*)pCurIn;
+            pCurIn += 4;*/
+
+            pPuppet->aTracks[i].curFrame = SITHDSS_POPFLOAT();
+            /*pPuppet->aTracks[i].curFrame = *(float*)pCurIn;
+            pCurIn += 4;*/
+
+            pPuppet->aTracks[i].prevFrame = SITHDSS_POPFLOAT();
+            /*pPuppet->aTracks[i].prevFrame = *(float*)pCurIn;
+            pCurIn += 4;*/
+
+            size_t cbIdx = SITHDSS_POPUINT8();
+            //size_t cbIdx = *pCurIn++;
+            if ( cbIdx < STD_ARRAYLEN(sithDSS_aPuppetCallbacks) )
+            {
+                pPuppet->aTracks[i].pfCallback = sithDSS_aPuppetCallbacks[cbIdx][0];
+            }
+            else
+            {
+                pPuppet->aTracks[i].pfCallback = NULL;
+            }
+        }
+    }
+
+    if ( !pThing->pPuppetState )
+    {
+        SITHDSS_ENDIN;
+        return 1;
+    }
+
+    uint32_t armMode = SITHDSS_POPINT16();
+    /*uint32_t armMode = *(int16_t*)pCurIn;
+    pCurIn += 2;*/
+
+    sithPuppet_SetArmedMode(pThing, armMode);
+
+    SithPuppetMoveMode moveMode = SITHDSS_POPUINT16();
+    /*SithPuppetMoveMode moveMode = *(uint16_t*)pCurIn;
+    pCurIn += 2;*/
+
+    int bControlsDisabled = false;
+    if ( pThing->type == SITH_THING_PLAYER || pThing->type == SITH_THING_ACTOR )
+    {
+        bControlsDisabled = pThing->thingInfo.actorInfo.bControlsDisabled;
+    }
+
+    sithPuppet_SetMoveMode(pThing, moveMode);
+    if ( pThing->type == SITH_THING_PLAYER || pThing->type == SITH_THING_ACTOR )
+    {
+        pThing->thingInfo.actorInfo.bControlsDisabled = bControlsDisabled;
+    }
+
+    size_t trackCount = SITHDSS_POPUINT16();
+    /*size_t trackCount = *(uint16_t*)pCurIn;
+    pCurIn += 2;*/
+
+    while ( trackCount )
+    {
+        size_t trackNum = SITHDSS_POPINT32();
+        /*size_t trackNum = *(int32_t*)pCurIn;
+        pCurIn += 4;*/
+
+        SithPuppetSubMode submode = SITHDSS_POPUINT32();
+        /*SithPuppetSubMode submode = *(uint32_t*)pCurIn;
+        pCurIn += 4;*/
+
+        if ( trackNum < STD_ARRAYLEN(pPuppet->aTracks) && sithPuppet_NewTrack(pThing, &pThing->pPuppetClass->aModes[pThing->pPuppetState->majorMode][submode], trackNum, submode) )
+        {
+            SITHDSS_ENDIN;
+            return 0;
+        }
+
+        --trackCount;
+    }
+
+    SITHDSS_ENDIN;
+    return 1;
 }
--- Libs/sith/Dss/sithDSS.h
+++ Libs/sith/Dss/sithDSS.h
@@ -1,43 +1,347 @@
 #ifndef SITH_SITHDSS_H
 #define SITH_SITHDSS_H
 #include <j3dcore/j3d.h>
+
+#include <rdroid/Math/rdMatrix.h>
+#include <rdroid/Math/rdVector.h>
 #include <rdroid/types.h>
+
+#include <sith/Dss/sithMulti.h>
 #include <sith/types.h>
 #include <sith/RTI/addresses.h>
+
 #include <std/types.h>
 
 J3D_EXTERN_C_START
 
+//Helper macros for data serialization / de-serialization
+
+#define SITHDSS_STARTOUT(t) SITHDSS_STARTOUTEX(&sithMulti_g_message, t)
+#define SITHDSS_STARTOUTEX(msg, t) SithMessage* pCurDSSMsg_ = msg; pCurDSSMsg_->type = t; uint8_t* pCurDSSMsgData_ = &pCurDSSMsg_->data[0]
+#define SITHDSS_ENDOUT (pCurDSSMsg_->length = (uintptr_t)pCurDSSMsgData_- (uintptr_t)&pCurDSSMsg_->data[0])
+
+#define SITHDSS_STARTIN(msg) uint8_t* pCurDSSMsgData_ = (uint8_t*)&(msg)->data[0]
+#define SITHDSS_ENDIN
+
+#define SITHDSS_PUSHINT8(x) sithDSS_WriteInt8(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHUINT8(x) sithDSS_WriteUint8(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHINT16(x) sithDSS_WriteInt16(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHUINT16(x) sithDSS_WriteUint16(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHINT32(x) sithDSS_WriteInt32(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHUINT32(x) sithDSS_WriteUint32(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHFLOAT(x) sithDSS_WriteFloat(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHVECT2(x) sithDSS_WriteVec2(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHVEC3(x) sithDSS_WriteVec3(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHVECT4(x) sithDSS_WriteVec4(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHMAT34(x) sithDSS_WriteMat34(&pCurDSSMsgData_, (x))
+#define SITHDSS_PUSHSTRING(x, len) sithDSS_WriteString(&pCurDSSMsgData_, (x), (len))
+#define SITHDSS_PUSHWSTRING(x, len) sithDSS_WriteWString(&pCurDSSMsgData_, (x), (len))
+
+#define SITHDSS_POPINT8() sithDSS_ReadInt8(&pCurDSSMsgData_)
+#define SITHDSS_POPUINT8() sithDSS_ReadUint8(&pCurDSSMsgData_)
+#define SITHDSS_POPINT16() sithDSS_ReadInt16(&pCurDSSMsgData_)
+#define SITHDSS_POPUINT16() sithDSS_ReadUint16(&pCurDSSMsgData_)
+#define SITHDSS_POPINT32() sithDSS_ReadInt32(&pCurDSSMsgData_)
+#define SITHDSS_POPUINT32() sithDSS_ReadUint32(&pCurDSSMsgData_)
+#define SITHDSS_POPFLOAT() sithDSS_ReadFloat(&pCurDSSMsgData_)
+#define SITHDSS_POPVECT2(x) sithDSS_ReadVec2(&pCurDSSMsgData_, (x))
+#define SITHDSS_POPVEC3(x) sithDSS_ReadVec3(&pCurDSSMsgData_, (x))
+#define SITHDSS_POPVECT4(x) sithDSS_ReadVec4(&pCurDSSMsgData_, (x))
+#define SITHDSS_POPMAT34(x) sithDSS_ReadMat34(&pCurDSSMsgData_, (x))
+#define SITHDSS_POPSTRING(x, len)  sithDSS_ReadString(&pCurDSSMsgData_, (x), (len))
+#define SITHDSS_POPWSTRING(x, len) sithDSS_ReadWString(&pCurDSSMsgData_, (x), (len))
+
+
+typedef enum eSithDSSType
+{
+    SITHDSS_POS                 = 1,
+    SITHDSS_CHAT                = 2,
+    SITHDSS_SECTORFLAGS         = 3,
+    SITHDSS_FIRE                = 4,
+    SITHDSS_DEATH               = 5,
+    SITHDSS_DAMAGE              = 6,
+    SITHDSS_SETMODEL            = 7,
+    SITHDSS_COGMESSAGE          = 8,
+    SITHDSS_PLAYKEY             = 9,
+    SITHDSS_PLAYSOUND           = 10,
+    SITHDSS_THINGSTATE          = 11,
+    SITHDSS_THINGFULLDESC       = 12,
+    SITHDSS_COGSTATE            = 13,
+    SITHDSS_SURFACESTATUS       = 14,
+    SITHDSS_AISTATUS            = 15,
+    SITHDSS_INVENTORY           = 16,
+    SITHDSS_ANIMSTATUS          = 17,
+    SITHDSS_SECTORSTATUS        = 18,
+    SITHDSS_PLAYKEYMODE         = 19,
+    SITHDSS_PATHMOVE            = 20,
+    SITHDSS_PUPPETSTATUS        = 21,
+    SITHDSS_ATTACHMENT          = 22,
+    SITHDSS_SYNCTASKEVENTS      = 23,
+    SITHDSS_SYNCCAMERAS         = 24,
+    SITHDSS_TAKE                = 25,
+    SITHDSS_TAKE2               = 26,
+    SITHDSS_STOPKEY             = 27,
+    SITHDSS_STOPSOUND           = 28,
+    SITHDSS_CREATETHING         = 29,
+    SITHDSS_WHIPSTATUS          = 30,
+    SITHDSS_SYNCWORLDSTATE      = 31,
+    SITHDSS_SYNCPLAYERS         = 32,
+    SITHDSS_WELCOME             = 33,
+    SITHDSS_JOIN                = 34,
+    SITHDSS_DESTROYTHING        = 35,
+    SITHDSS_PLAYSOUNDMODE       = 37,
+    SITHDSS_PING                = 38,
+    SITHDSS_PONG                = 39,
+    SITHDSS_MOVEPOS             = 40,
+    SITHDSS_CHECKMEMBERSHIPLIST = 41,
+    SITHDSS_QUIT                = 42,
+    SITHDSS_UNKNOWN_44          = 44,
+    SITHDSS_VOICESTATE          = 45,
+    SITHDSS_VEHICLECONTROLSTATE = 46,
+    SITHDSS_SYNCWPNT            = 48,
+    SITHDSS_SYNCSEENSECTORS     = 49,
+} SithDSSType;
+
 int J3DAPI sithDSS_SurfaceStatus(const SithSurface* pSurf, DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessSurfaceStatus(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_SectorStatus(const SithSector* pSector, DPID a2, unsigned int outstream);
 int J3DAPI sithDSS_SyncSeenSectors(DPID idTo, unsigned int outstream);
+
 int J3DAPI sithDSS_ProcessSyncSeenSecors(const SithMessage* pMsg);
 int J3DAPI sithDSS_ProcessSectorStatus(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_SectorFlags(const SithSector* pSector, DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessSectorFlags(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_AIStatus(const SithAIControlBlock* pLocal, DPID idTo, signed int outstream);
 int J3DAPI sithDSS_ProcessAIStatus(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_Inventory(const SithThing* pThing, unsigned int inventoryId, DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessInventory(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_AnimStatus(const SithAnimationSlot* pAnim, DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessAnimStatus(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_PuppetStatus(const SithThing* pThing, DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessPuppetStatus(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_SyncTaskEvents(const SithEvent* pEvent, DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessSyncTaskEvents(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_SyncCameras(DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessSyncCameras(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_SyncWorldState(DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessSyncWorldState(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_SyncVehicleControlState(DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_ProcessVehicleControlsState(const SithMessage* pMsg);
+
 int J3DAPI sithDSS_sub_4B3760(DPID idTo, unsigned int outstream);
 int J3DAPI sithDSS_sub_4B3790(const SithMessage* pMsg);
+
+// Functions for de-serializing data from stream
+int8_t J3DAPI sithDSS_ReadInt8(uint8_t** stream);
+uint8_t J3DAPI sithDSS_ReadUint8(uint8_t** stream);
+int16_t J3DAPI sithDSS_ReadInt16(uint8_t** stream);
+uint16_t J3DAPI sithDSS_ReadUint16(uint8_t** stream);
+int32_t J3DAPI sithDSS_ReadInt32(uint8_t** stream);
+uint32_t J3DAPI sithDSS_ReadUint32(uint8_t** stream);
+float J3DAPI sithDSS_ReadFloat(uint8_t** stream);
+void J3DAPI sithDSS_ReadVec2(uint8_t** stream, rdVector2* vec);
+void J3DAPI sithDSS_ReadVec3(uint8_t** stream, rdVector3* vec);
+void J3DAPI sithDSS_ReadVec4(uint8_t** stream, rdVector4* vec);
+void J3DAPI sithDSS_ReadMat34(uint8_t** stream, rdMatrix34* mat);
+void J3DAPI sithDSS_ReadString(uint8_t** stream, char* str, int length);
+void J3DAPI sithDSS_ReadWString(uint8_t** stream, wchar_t* str, int length);
+
+// Functions for serializing data to stream
+void J3DAPI sithDSS_WriteInt8(uint8_t** stream, int8_t val);
+void J3DAPI sithDSS_WriteUint8(uint8_t** stream, uint8_t val);
+void J3DAPI sithDSS_WriteInt16(uint8_t** stream, int16_t val);
+void J3DAPI sithDSS_WriteUint16(uint8_t** stream, uint16_t val);
+void J3DAPI sithDSS_WriteInt32(uint8_t** stream, int32_t val);
+void J3DAPI sithDSS_WriteUint32(uint8_t** stream, uint32_t val);
+void J3DAPI sithDSS_WriteFloat(uint8_t** stream, float val);
+void J3DAPI sithDSS_WriteVec2(uint8_t** stream, const rdVector2* vec);
+void J3DAPI sithDSS_WriteVec3(uint8_t** stream, const rdVector3* vec);
+void J3DAPI sithDSS_WriteVec4(uint8_t** stream, const rdVector4* vec);
+void J3DAPI sithDSS_WriteMat34(uint8_t** stream, const rdMatrix34* mat);
+void J3DAPI sithDSS_WriteString(uint8_t** stream, const char* str, int length);
+void J3DAPI sithDSS_WriteWString(uint8_t** stream, const wchar_t* str, int length);
 
 // Helper hooking functions
 void sithDSS_InstallHooks(void);
 void sithDSS_ResetGlobals(void);
+
+
+// Serialization functions
+
+inline void sithDSS_WriteInt8(uint8_t** stream, int8_t val)
+{
+    **stream = val;
+    (*stream) += sizeof(int8_t);
+}
+
+inline void sithDSS_WriteUint8(uint8_t** stream, uint8_t val)
+{
+    **stream = val;
+    *stream += sizeof(uint8_t);
+}
+
+inline void sithDSS_WriteInt16(uint8_t** stream, int16_t val)
+{
+    *(int16_t*)*stream = val;
+    *stream += sizeof(int16_t);
+}
+
+inline void sithDSS_WriteUint16(uint8_t** stream, uint16_t val)
+{
+    *(uint16_t*)*stream = val;
+    *stream += sizeof(uint16_t);
+}
+
+inline void sithDSS_WriteInt32(uint8_t** stream, int32_t val)
+{
+    *(int32_t*)*stream = val;
+    *stream += sizeof(int32_t);
+}
+
+inline void sithDSS_WriteUint32(uint8_t** stream, uint32_t val)
+{
+    *(uint32_t*)*stream = val;
+    *stream += sizeof(uint32_t);
+}
+
+inline void sithDSS_WriteFloat(uint8_t** stream, float val)
+{
+    *(float*)*stream = val;
+    *stream += sizeof(float);
+}
+
+inline void sithDSS_WriteVec2(uint8_t** stream, const rdVector2* vec)
+{
+    rdVector_Copy2((rdVector2*)*stream, vec);
+    *stream += sizeof(rdVector2);
+}
+
+inline void sithDSS_WriteVec3(uint8_t** stream, const rdVector3* vec)
+{
+    rdVector_Copy3((rdVector3*)*stream, vec);
+    *stream += sizeof(rdVector3);
+}
+
+inline void sithDSS_WriteVec4(uint8_t** stream, const rdVector4* vec)
+{
+    rdVector_Copy4((rdVector4*)*stream, vec);
+    *stream += sizeof(rdVector4);
+}
+
+inline void sithDSS_WriteMat34(uint8_t** stream, const rdMatrix34* mat)
+{
+    rdMatrix_Copy34((rdMatrix34*)*stream, mat);
+    *stream += sizeof(rdMatrix34);
+}
+
+inline void sithDSS_WriteString(uint8_t** stream, const char* str, int length)
+{
+    memcpy(*stream, str, length);
+    *stream += length;
+}
+
+inline void sithDSS_WriteWString(uint8_t** stream, const wchar_t* str, int length)
+{
+    memcpy(*stream, str, length * sizeof(wchar_t));
+    *stream += length * sizeof(wchar_t);
+}
+
+// De-serialization functions
+
+inline int8_t sithDSS_ReadInt8(uint8_t** stream)
+{
+    int8_t data = *(int8_t*)*stream;
+    *stream += sizeof(int8_t);
+    return data;
+}
+
+inline uint8_t sithDSS_ReadUint8(uint8_t** stream)
+{
+    uint8_t data = **stream;
+    *stream += sizeof(uint8_t);
+    return data;
+}
+
+inline int16_t sithDSS_ReadInt16(uint8_t** stream)
+{
+    int16_t data = *(int16_t*)*stream;
+    *stream += sizeof(int16_t);
+    return data;
+}
+
+inline uint16_t sithDSS_ReadUint16(uint8_t** stream)
+{
+    uint16_t data = *(uint16_t*)*stream;
+    *stream += sizeof(uint16_t);
+    return data;
+}
+
+inline int32_t sithDSS_ReadInt32(uint8_t** stream)
+{
+    int32_t data = *(int32_t*)*stream;
+    *stream += sizeof(int32_t);
+    return data;
+}
+
+inline uint32_t sithDSS_ReadUint32(uint8_t** stream)
+{
+    uint32_t data = *(uint32_t*)*stream;
+    *stream += sizeof(uint32_t);
+    return data;
+}
+
+inline float sithDSS_ReadFloat(uint8_t** stream)
+{
+    float data = *(float*)*stream;
+    *stream += sizeof(float);
+    return data;
+}
+
+inline void sithDSS_ReadVec2(uint8_t** stream, rdVector2* vec)
+{
+    rdVector_Copy2(vec, (rdVector2*)*stream);
+    *stream += sizeof(rdVector2);
+}
+
+inline void sithDSS_ReadVec3(uint8_t** stream, rdVector3* vec)
+{
+    rdVector_Copy3(vec, (rdVector3*)*stream);
+    *stream += sizeof(rdVector3);
+}
+
+inline void sithDSS_ReadVec4(uint8_t** stream, rdVector4* vec)
+{
+    rdVector_Copy4(vec, (rdVector4*)*stream);
+    *stream += sizeof(rdVector4);
+}
+
+inline void sithDSS_ReadMat34(uint8_t** stream, rdMatrix34* mat)
+{
+    rdMatrix_Copy34(mat, (rdMatrix34*)*stream);
+    *stream += sizeof(rdMatrix34);
+}
+
+inline void sithDSS_ReadString(uint8_t** stream, char* str, int length)
+{
+    memcpy(str, *stream, length);
+    *stream += length;
+}
+
+inline void sithDSS_ReadWString(uint8_t** stream, wchar_t* str, int length)
+{
+    memcpy(str, *stream, length * sizeof(wchar_t));
+    *stream += length * sizeof(wchar_t);
+}
 
 J3D_EXTERN_C_END
 #endif // SITH_SITHDSS_H
