0bb96dc184aff38194e3e9036ba91d76a93e85a2
Crt Vavros <4377556+smlu@users.noreply.github.com>
2024-10-05 14:48:04 +0200

    [rdroid] Add missing function definitions

--- Libs/rdroid/Engine/rdLight.h
+++ Libs/rdroid/Engine/rdLight.h
@@ -7,9 +7,17 @@
 
 J3D_EXTERN_C_START
 
+rdLight* rdLight_New(void); // Added
 int J3DAPI rdLight_NewEntry(rdLight* pLight);
+void J3DAPI rdLight_Free(rdLight* pLight); // Added
+void rdLight_FreeEntry(rdLight* pLight); // Added
+
 void J3DAPI rdLight_CalcVertexIntensities(const rdLight** apLights, const rdVector3* aLightPos, size_t numLights, const rdVector3* aVertexNormal, const rdVector3* aVertices, const rdVector4* aVertexColors, rdVector4* aLightColors, size_t numVertices);
+void J3DAPI rdLight_CalcDistVertexIntensities(const rdLight** apLights, const rdVector3* aLightPos, size_t numLights, const rdVector3* aVertexNormal, const rdVector3* aVertices, const rdVector4* aVertexColors, rdVector4* aLightColors, size_t numVertices, float attenuation); // Added
+
 void J3DAPI rdLight_CalcFaceIntensity(const rdLight** apLights, const rdVector3* apLightPos, size_t numLights, const rdFace* pFace, const rdVector3* pNormal, const rdVector3* apVertices, float attenuation, rdVector4* pOutLightColor);
+void J3DAPI rdLight_CalcDistFaceIntensity(const rdLight** apLights, const rdVector3* apLightPos, size_t numLights, const rdFace* pFace, const rdVector3* aVerts, float attenuation, rdVector4* pOutColor); // Added
+
 float J3DAPI rdLight_GetIntensity(const rdVector4* pLight);
 
 // Helper hooking functions
