e46c2abc35085deabdfaf2254b42fae7789d9d63
Crt Vavros <4377556+smlu@users.noreply.github.com>
2025-01-30 00:55:55 +0100

    [sith] Implement `sithCogFunctionSector`

--- Libs/sith/Cog/sithCogFunctionSector.h
+++ Libs/sith/Cog/sithCogFunctionSector.h
@@ -10,30 +10,6 @@
 J3D_EXTERN_C_START
 
 int J3DAPI sithCogFunctionSector_RegisterFunctions(SithCogSymbolTable* pTable);
-void J3DAPI sithCogFunctionSector_GetSectorTint(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SetSectorTint(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorLight(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SetSectorLight(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SetSectorAdjoins(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SetSectorSurfflags(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_ClearSectorSurfflags(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorColormap(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SetSectorColormap(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorThrust(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SetSectorThrust(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SetSectorFlags(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_ClearSectorFlags(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorFlags(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorThingCount(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorPlayerCount(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorCount(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorCenter(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetNumSectorVertices(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetNumSectorSurfaces(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorVertexPos(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_GetSectorSurfaceRef(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_SyncSector(SithCog* pCog);
-void J3DAPI sithCogFunctionSector_FindSectorAtPos(SithCog* pCog);
 
 // Helper hooking functions
 void sithCogFunctionSector_InstallHooks(void);
